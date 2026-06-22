45fec0c46a1f0415fe4f357dd9761082e20044bf
Kahtaf Alam <kahtaf@gmail.com>
2026-06-22 15:59:38 -0400

    Guard GUI startup before writer lock

--- gui/server/server.ts
+++ gui/server/server.ts
@@ -11,6 +11,7 @@ import {
   SettingsManager,
 } from "@earendil-works/pi-coding-agent";
 import { createOpenCandleSession } from "../../src/index.js";
+import { assertSupportedNodeVersion } from "../../src/infra/node-version.js";
 import { createAskUserBridge } from "./ask-user-bridge.js";
 import {
   createLocalAutomationHeartbeat,
@@ -32,6 +33,8 @@ import { createSessionActionsController } from "./session-actions.js";
 import { createGracefulShutdown } from "./shutdown.js";
 import { acquireWriterLock, refreshWriterLock, releaseWriterLock } from "./writer-lock.js";
 import { createWsHub, type WsHub } from "./ws-hub.js";
+
+assertSupportedNodeVersion();
 
 const cwd = process.cwd();
 const host = process.env.OPENCANDLE_GUI_HOST ?? "127.0.0.1";
